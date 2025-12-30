1c232f1a8911e759ed1b88e7bc47fd2786b9a65f
luigi-budd <90123055+luigi-budd@users.noreply.github.com>
2025-12-30 03:59:16 -0600

    update pain graphics

--- mod/Lua/hud/drawers/painsurge.lua
+++ mod/Lua/hud/drawers/painsurge.lua
@@ -4,7 +4,7 @@ local paintics = 0
 function HUD:painSurge(p)
 	if displayplayer ~= p then return end
 	
-	paintics = 4
+	paintics = 6
 end
 
 addHook("HUD",function(v,p,cam)
@@ -12,14 +12,13 @@ addHook("HUD",function(v,p,cam)
 	if not Paint:playerIsActive(p) then return end
 	if not paintics then return end
 	
-	local frame = paintics
+	local frame = (7 - paintics)
 	local patch = v.cachePatch("PAINT_PS_"..(frame))
 	local wid = (v.width() / v.dupx()) + 1
 	local hei = (v.height() / v.dupy()) + 1
 	local p_w = patch.width
 	local p_h = patch.height
-	local nudge = FU/2
-	v.drawStretched(nudge,nudge,
+	v.drawStretched(0,0,
 		FixedDiv(wid * FU, p_w * FU),
 		FixedDiv(hei * FU, p_h * FU),
 		patch,
