8e6834843a3930115383feb3e4930bb656c84ba7
luigi-budd <90123055+luigi-budd@users.noreply.github.com>
2026-02-14 16:50:26 -0600

    no turf points in hide and seek

--- mod/Lua/weapons/bullet.lua
+++ mod/Lua/weapons/bullet.lua
@@ -116,6 +116,7 @@ local SPLAT_MAXSIZE = (SPLAT_SIZEMUL)*2
 local function AddTurfPoints(p, points)
 	p.paint.turfpoints = $ + points
 	if (p.paint.turfpoints % 4 == 1)
+	and not (gametyperules & (GTR_TAG|GTR_HIDEFROZEN) == (GTR_TAG|GTR_HIDEFROZEN))
 		P_AddPlayerScore(p, points)
 	end
 end
