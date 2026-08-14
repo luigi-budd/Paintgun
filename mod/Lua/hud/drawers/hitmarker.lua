94d9fcdc5959eef671f450e290c8d9acf5c90734
luigi-budd <90123055+luigi-budd@users.noreply.github.com>
2026-08-13 21:42:39 -0500

    Add powerful hitmarkers

--- mod/Lua/hud/drawers/hitmarker.lua
+++ mod/Lua/hud/drawers/hitmarker.lua
@@ -2,6 +2,8 @@ local HUD = Paint.HUD
 local offset = 0
 
 freeslot("SPR_PAINT_HITMARKER")
+local POWERFUL_LEN = TR/2
+local POWERFUL_FIRST = 4
 function HUD:hitMarker(p, pos, rotangle, sizemul, powerful, blocked)
 	if displayplayer ~= p then return end
 	
@@ -9,9 +11,16 @@ function HUD:hitMarker(p, pos, rotangle, sizemul, powerful, blocked)
 		HUD.memory.hitmarkers = {}
 	end
 	
+	local tics = 4
+	if (blocked)
+		tics = 8
+	elseif powerful
+		tics = POWERFUL_LEN
+	end
 	table.insert(HUD.memory.hitmarkers, {
 		pos = pos, --{x=mo.x,y=mo.y,z=mo.z + mo.height/2},
-		tics = blocked and 8 or 4,
+		tics = tics,
+		elapsed = 0,
 		frame = A,
 		angle = rotangle or 0,
 		sizemul = sizemul or FU,
@@ -25,6 +34,40 @@ end
 local function Icon(v,p,cam, info)
 	local result = K_GetScreenCoords(v,p,cam, info.pos)
 	if not result.onscreen then return end
+	local scale = FixedMul(FU/2, info.sizemul)
+	
+	if info.powerful
+		local circ = v.getSpritePatch(SPR_PAINT_HITMARKER, 4, 0,0)
+		local el = info.elapsed
+		if el < POWERFUL_FIRST
+			local adjust = FU * (POWERFUL_FIRST - el)
+			v.drawScaled(result.x,result.y, FixedMul(scale, adjust),
+				circ, V_ADD)
+		elseif el > POWERFUL_FIRST
+			local ANIM = POWERFUL_LEN - POWERFUL_FIRST
+			local frac = ease.outexpo(((FU/ANIM)*(el - POWERFUL_FIRST)), FU, 0)
+			v.drawStretched(result.x, result.y,
+				FixedMul(10*scale, FU - frac), FixedMul(scale*3, frac or FU),
+				circ, V_ADD)
+		end
+		if el <= POWERFUL_FIRST
+			local adjust = FU
+			if el == 1
+				adjust = FU * 3/2
+			elseif el == 2
+				adjust = FU*6/3
+			elseif el == 3
+				adjust = FU + (FU/6)
+			end
+			
+			local patch = v.getSpritePatch(SPR_PAINT_HITMARKER, 5 + el, 0, info.angle)
+			v.drawScaled(result.x,result.y, FixedMul(scale*3/2, adjust),
+				patch,
+				0, v.getColormap(nil, Paint:getPlayerColor(p))
+			)
+		end
+		return
+	end
 	
 	local flags = 0
 	local patch
@@ -35,7 +78,6 @@ local function Icon(v,p,cam, info)
 		patch = v.getSpritePatch(SPR_PAINT_HITMARKER, clamp(0,info.frame,3), 0, info.angle)
 	end
 	if patch == nil then return end
-	local scale = FixedMul(FU/2, info.sizemul)
 	
 	v.drawScaled(result.x,result.y, scale, --scale,
 		patch,
@@ -48,23 +90,19 @@ addHook("HUD",function(v,p,cam)
 	local feed = HUD.memory.hitmarkers
 	if feed == nil then return end
 	
-	local toremove = {}
-	for k, info in ipairs(feed)
+	for i = #feed, 1, -1
+		local info = feed[i]
 		if info.tics <= 0
-			table.insert(toremove, k)
+			table.remove(feed, i); continue
 		end
-	end
-	for _, k in ipairs(toremove)
-		table.remove(feed, k)
-	end
-	
-	for k, info in ipairs(feed)
+		
 		v.dointerp(info.interptag)
 		Icon(v,p,cam, info)
 		v.dointerp(false)
 		if not paused
 			info.tics = $ - 1
 			info.frame = $ + 1
+			info.elapsed = $ + 1
 		end
 	end
 	offset = 0
