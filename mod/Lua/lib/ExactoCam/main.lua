5fcd1612f5287b3d5693bf562f3e4312947f623b
luigi-budd <90123055+luigi-budd@users.noreply.github.com>
2026-08-29 14:59:12 -0500

    update exactocam

--- mod/Lua/lib/ExactoCam/main.lua
+++ mod/Lua/lib/ExactoCam/main.lua
@@ -501,5 +501,6 @@ end)
 
 addHook("PostThinkFrame",do
 	if (gamestate ~= GS_LEVEL) then return end
+	if (ExactoCam_Version > MYVERSION) then return end
 	ExactoCam_Thinker(displayplayer, camera)
 end)
