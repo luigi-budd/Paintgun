294a4bb28449bb544bfdf0eda5f71af255a787df
luigi-budd <90123055+luigi-budd@users.noreply.github.com>
2026-09-23 19:04:01 -0500

    Update knockback.lua
    Updated from Zombie Escape 2

--- mod/Lua/lib/knockback.lua
+++ mod/Lua/lib/knockback.lua
@@ -2,95 +2,128 @@ rawset(_G,"Knockback",{})
 local KB = Knockback
 
 KB.list = {}
-addHook("NetVars",function(n)
-	KB.list = n($)
-end)
-addHook("MapLoad",do
-	KB.list = {}
-end)
 
-KB.initKnockback = function(mo)
+function KB.initKnockback(mo)
 	mo.knockback = {
 		list = {},
-		thrust = {x=0,y=0},
-		wait = 0,
+		thrust = {x=0,y=0}
 	}
 end
 
-KB.addKnockback = function(mo, tics, angle, thrust)
-	if not mo.knockback
+function KB.addKnockback(mo, tics, angle, thrust)
+	if not mo.knockback then
 		KB.initKnockback(mo)
 	end
-	if mo.knockback.wait then return end
+
 	table.insert(mo.knockback.list, {
 		tics = tics,
 		angle = angle,
 		thrust = thrust,
 		frac = (FU / tics),
 	})
-	mo.knockback.wait = 3
+
 	local wasinlist = false
-	for id,othermo in ipairs(KB.list)
-		if othermo == mo
+	for id,othermo in ipairs(KB.list) do
+		if othermo == mo then
 			wasinlist = true
 			break
 		end
 	end
-	if not wasinlist
+
+	if not wasinlist then
 		table.insert(KB.list, mo)
 	end
 end
 
-addHook("ThinkFrame",do
-	if gamestate ~= GS_LEVEL then return end
+addHook("NetVars", function(net)
+	KB.list = net($)
+end)
+
+addHook("MapChange", function()
+	for i,v in pairs(KB.list) do
+		KB.list[i] = nil
+	end
+end)
+
+local function validKBMobj(mo)
+	return (mo and mo.valid) and (mo.health) and (mo.knockback ~= nil) and (#mo.knockback.list)
+end
+
+addHook("ThinkFrame", function()
+	if gamestate ~= GS_LEVEL then
+		return end;
+
 	--clean up
-	for k,mo in ipairs(KB.list)
-		if not (
-			(mo and mo.valid)
-			and (mo.health)
-			and (mo.knockback ~= nil)
-			and (#mo.knockback.list)
-		) then
-			if (mo and mo.valid and mo.knockback)
-				mo.knockback = nil
+	if #KB.list then
+		for k = #KB.list, 1, -1 do
+			local mo = KB.list[k]
+
+			if not validKBMobj(mo) then
+				if (mo and mo.valid and mo.knockback) then
+					mo.knockback = nil
+				end
+
+				table.remove(KB.list,k)
 			end
-			table.remove(KB.list,k)
 		end
 	end
+
 	--iterate
-	for k,mo in ipairs(KB.list)
-		local thrust = {x = 0,y = 0}
+	for k,mo in ipairs(KB.list) do
+		local thrust = {x = 0; y = 0}
 		local knocked = false
-		
+
+		if not validKBMobj(mo) then -- just in case
+			continue
+		end
+
 		local grounded = P_IsObjectOnGround(mo)
 		local k = mo.knockback
-		for id,t in ipairs(k.list)
+
+		-- Clean up inactive knockback tables.
+		for id,t in ipairs(k.list) do
 			if not t.tics then table.remove(k.list, id); end
 		end
-		for id,t in ipairs(k.list)
+
+		for id,t in ipairs(k.list) do
 			local force = ease.outcubic(FU - (t.frac * t.tics), t.thrust, 0)
-			if grounded
+			if grounded then
 				force = FixedDiv($, mo.friction)
 			end
 			thrust.x = $ + P_ReturnThrustX(nil,t.angle, force)
 			thrust.y = $ + P_ReturnThrustY(nil,t.angle, force)
 			t.tics = $ - 1
-			if not t.didit
-				local frac = FU * 5/6
+			if not t.didit then
+				local frac = FU/2
 				mo.momx,mo.momy = FixedMul($1,frac),FixedMul($2,frac)
 				t.didit = true
 			end
 			knocked = true
 		end
+
 		local accspeed = FixedDiv(abs(FixedHypot(mo.momx,mo.momy)), mo.scale)
-		if knocked
-			P_TryMove(mo,
+		if knocked then
+			local moved = P_TryMove(mo,
 				mo.x + thrust.x,
 				mo.y + thrust.y,
 				true
 			)
+
+			-- Don't get stuck on a wall!
+			if not moved then
+				-- give momentum so slidemove can work
+				mo.momx = thrust.x
+				mo.momy = thrust.y
+
+				P_SlideMove(mo)
+
+				-- we dont need momentum anymore since we werent already moving
+				mo.momx = 0
+				mo.momy = 0
+			end
+
 			local cap = 25*FU
-			if accspeed > cap
+			if accspeed > cap then
 				local newspeed = accspeed - FixedDiv(accspeed - cap, 8*FU)
 				newspeed = FixedMul($,mo.scale)
 				local ang = R_PointToAngle2(0,0,mo.momx,mo.momy)
@@ -98,7 +131,5 @@ addHook("ThinkFrame",do
 				mo.momy = P_ReturnThrustY(nil,ang,newspeed)
 			end
 		end
-		
-		k.wait = max($-1, 0)
 	end
-end)	
+end)
